c546a39bf3942556f55420362b83384418a7930b
Alexey V.Ryapisov <tealikes@yandex.ru>
2021-01-11 03:03:21 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+<p align="center">
+  Template: Carolina
+</p>
---
+++ src/example-components/Maps/MapsLeafletMaps/index.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react';
+
+export default function LivePreviewExample() {
+  const lat = 51.505;
+  const lng = -0.09;
+  const zoom = 5;
+
+  const position = [lat, lng];
+
+  return (
+    <Fragment>12345
+      {/* <Map center={position} zoom={zoom}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </Map> */}
+    </Fragment>
+  );
+}
